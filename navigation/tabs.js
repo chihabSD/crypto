32ab13a95b104baa4bde472607fe4126f2021615
chihabsd <adamchihab2012@hotmail.com>
2021-09-30 10:53:43 +0100

    customizing bottom tabs icons and labels

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,47 +1,89 @@
 import React from "react";
-import {
-    TouchableOpacity,
-} from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { TouchableOpacity } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { Home, Portfolio, Market, Profile } from "../screens"
-import { COLORS } from "../constants"
-
-const Tab = createBottomTabNavigator()
+import { Home, Portfolio, Market, Profile } from "../screens";
+import { COLORS, icons } from "../constants";
+import TabIcon from "../components/TabIcon";
+// import { TabIcon } from "../components";
+const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-
-    return (
-        <Tab.Navigator
-            tabBarOptions={{
-                style: {
-                    backgroundColor: COLORS.primary,
-                    borderTopColor: "transparent",
-                }
-            }}
-        >
-            <Tab.Screen
-                name="Home"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Portfolio"
-                component={Portfolio}
-            />
-            <Tab.Screen
-                name="Trade"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Market"
-                component={Market}
-            />
-            <Tab.Screen
-                name="Profile"
-                component={Profile}
-            />
-        </Tab.Navigator>
-    )
-}
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          height: 140,
+          backgroundColor: COLORS.primary,
+          borderTopColor: "transparent",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return <TabIcon focused={focused} label="home" icon={icons.home} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Portfolio"
+        component={Portfolio}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                label="portfiolio"
+                icon={icons.briefcase}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Trade"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                label="trade"
+                icon={icons.trade}
+                isTrade={true}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Market"
+        component={Market}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon focused={focused} label="market" icon={icons.market} />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon focused={focused} label="profile" icon={icons.profile} />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
